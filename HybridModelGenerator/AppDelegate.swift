f99d9709c68a573edb5b63edb2e83a31b0a3b442
jeffreyvarner <8898J5ff!>
2015-04-21 06:40:03 -0400

    Initial Commit

---
+++ HybridModelGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HybridModelGenerator
+//
+//  Created by Jeffrey Varner on 4/21/15.
+//  Copyright (c) 2015 Pooksoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
